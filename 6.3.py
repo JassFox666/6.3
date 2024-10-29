117bae088cc68461536ddb8acbe308c6534e54cd
wjaso <wjason440@gmail.com>
2024-10-29 13:52:41 +0200

    comit

---
+++ 6.3.py
@@ -0,0 +1,15 @@
+def multiply_digits_until_single_digit(number):
+    while number > 9:
+        product = 1
+        for digit in str(number):
+            product *= int(digit)
+        number = product
+    return number
+
+# Приклади використання
+print(multiply_digits_until_single_digit(999))   # 2
+print(multiply_digits_until_single_digit(1000))  # 0
+print(multiply_digits_until_single_digit(423))   # 8
+print(multiply_digits_until_single_digit(33))    # 9
+print(multiply_digits_until_single_digit(25))    # 0
+print(multiply_digits_until_single_digit(1))
